794beb668caaf6a0f06d22a6a0fc9599e6caa39c
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-11 23:45:51 +0430

    Cleaned the boilerplate code

--- src/NotificationProvider.js
+++ src/NotificationProvider.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import AndroidNotification from "react-mui-android-notification";
 
 export default function MyComponent() {
 	return (
 		<div>
-			<Button>test</Button>
 			<AndroidNotification />
 		</div>
 	);
