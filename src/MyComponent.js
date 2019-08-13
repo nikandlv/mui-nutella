0d92d957323f46d4f0a4cb7f1250e2cc26d479f4
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-13 12:23:54 +0430

    Added sections and nutella class

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import AndroidNotification from "react-mui-android-notification";
+import Nutella from "./Nutella";
 
-export default function MyComponent() {
-	return (
-		<div>
-			<Button>test</Button>
-			<AndroidNotification />
-		</div>
-	);
+export default class MyComponent extends React.Component {
+	push() {
+		Nutella.push();
+	}
+
+	render() {
+		return (
+			<div>
+				<Button onClick={this.push}>test</Button>
+				<Nutella.View />
+			</div>
+		);
+	}
 }
---
+++ src/Nutella.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Whatshot from "@material-ui/icons/Whatshot";
+import AndroidNotification from "react-mui-android-notification";
+
+const Nutella = [
+	{
+		position: "top",
+		data: {
+			name: "Android Notification",
+			icon: <Whatshot />,
+			date: "Nov 6",
+			accent: "rgb(63, 81, 181)",
+			title: "Awesome Notification",
+			body: "this is an awesome notification !",
+			rounded: false,
+			variant: "oreo",
+		},
+	},
+];
+
+function NutellaSection(props) {
+	const { position } = this.props;
+	const items = Nutella.filter(item => item.position === position);
+	return (
+		<div className={props.position}>
+			{items.map((item, key) => (
+				<AndroidNotification {...item} key={key} />
+			))}
+		</div>
+	);
+}
+
+function View() {
+	return (
+		<React.Fragment>
+			<NutellaSection position="top" />
+			<NutellaSection position="bottom" />
+			<NutellaSection position="left" />
+			<NutellaSection position="right" />
+		</React.Fragment>
+	);
+}
+
+const Interface = {
+	push: (position, data) => {
+		Nutella.push({
+			position,
+			data,
+		});
+	},
+};
+
+export default {
+	View,
+	...Interface,
+};
