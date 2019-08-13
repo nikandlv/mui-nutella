d3e79908246ec76f30d99389a8e0a7699f6657b1
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-13 13:08:55 +0430

    Added animation to the open transition

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,13 +2,14 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import Nutella from "./Nutella";
 import Whatshot from "@material-ui/icons/Whatshot";
-
+let counter = 0;
 export default class MyComponent extends React.Component {
 	push() {
+		counter++;
 		Nutella.push(
-			"top",
+			"bottom-right",
 			{
-				name: "Android Notification",
+				name: "Android Notification " + counter,
 				icon: <Whatshot />,
 				date: "Nov 6",
 				accent: "rgb(63, 81, 181)",
--- src/Nutella.js
+++ src/Nutella.js
@@ -1,21 +1,78 @@
 import React from "react";
 import AndroidNotification from "react-mui-android-notification";
 import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { TransitionGroup, Transition } from "react-transition-group";
 
 const Nutella = [];
 let Update = () => [console.log("Nutella is not ready yet")];
+
+const useStyles = makeStyles({
+	wrapper: {
+		display: "flex",
+		flexDirection: "column",
+		width: 380,
+		position: "absolute",
+		padding: "1rem",
+		transition: "all 1s",
+		overflow: "hidden",
+		"&.top-left": {
+			left: 0,
+			top: 0,
+		},
+		"&.bottom-right": {
+			right: 0,
+			bottom: 0,
+		},
+	},
+	notification: {
+		transition: "all 1s",
+	},
+});
+
+const duration = 300;
+
+const defaultStyle = {
+	transition: `all ${duration}ms ease-in-out`,
+	maxHeight: 0,
+	opacity: 0,
+};
+
+const transitionStyles = {
+	entering: { maxHeight: 0, opacity: 0 },
+	entered: { maxHeight: "200px", opacity: 1 },
+};
+
+const FadeTransition = ({ children, in: inProp }) => (
+	<Transition in={inProp} timeout={duration} unmountOnExit={true} mountOnEnter={true}>
+		{state => (
+			<div
+				style={{
+					...defaultStyle,
+					...transitionStyles[state],
+				}}
+			>
+				{children}
+			</div>
+		)}
+	</Transition>
+);
+
 function NutellaSection(props) {
+	const styles = useStyles();
 	const items = Nutella.filter(item => item.position === props.position);
 	return (
-		<div className={props.position}>
+		<TransitionGroup className={`${styles.wrapper} ${props.position}`}>
 			{items.map((item, key) => (
-				<AndroidNotification {...item.data} key={key}>
-					{item.actions.map((action, key) => (
-						<React.Fragment key={key}>{action}</React.Fragment>
-					))}
-				</AndroidNotification>
+				<FadeTransition key={key} className={styles.notification}>
+					<AndroidNotification className="test" {...item.data}>
+						{item.actions.map((action, key) => (
+							<React.Fragment key={key}>{action}</React.Fragment>
+						))}
+					</AndroidNotification>
+				</FadeTransition>
 			))}
-		</div>
+		</TransitionGroup>
 	);
 }
 
@@ -28,10 +85,10 @@ class View extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
-				<NutellaSection position="top" />
-				<NutellaSection position="bottom" />
-				<NutellaSection position="left" />
-				<NutellaSection position="right" />
+				<NutellaSection position="top-left" />
+				<NutellaSection position="bottom-left" />
+				<NutellaSection position="top-right" />
+				<NutellaSection position="bottom-right" />
 			</React.Fragment>
 		);
 	}
