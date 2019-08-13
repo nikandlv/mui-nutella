b5b7a86ce853bcc857a3ba98b2fb5532f6bcd37f
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-13 12:38:56 +0430

    Added push and demo tests and actions

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,16 +1,30 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import Nutella from "./Nutella";
+import Whatshot from "@material-ui/icons/Whatshot";
 
 export default class MyComponent extends React.Component {
 	push() {
-		Nutella.push();
+		Nutella.push(
+			"top",
+			{
+				name: "Android Notification",
+				icon: <Whatshot />,
+				date: "Nov 6",
+				accent: "rgb(63, 81, 181)",
+				title: "Awesome Notification",
+				body: "this is an awesome notification !",
+				rounded: false,
+				variant: "oreo",
+			},
+			[<Button>Inject test!</Button>],
+		);
 	}
 
 	render() {
 		return (
 			<div>
-				<Button onClick={this.push}>test</Button>
+				<Button onClick={this.push}>Push top</Button>
 				<Nutella.View />
 			</div>
 		);
--- src/Nutella.js
+++ src/Nutella.js
@@ -1,52 +1,50 @@
 import React from "react";
-import Whatshot from "@material-ui/icons/Whatshot";
 import AndroidNotification from "react-mui-android-notification";
+import { Button } from "@material-ui/core";
 
-const Nutella = [
-	{
-		position: "top",
-		data: {
-			name: "Android Notification",
-			icon: <Whatshot />,
-			date: "Nov 6",
-			accent: "rgb(63, 81, 181)",
-			title: "Awesome Notification",
-			body: "this is an awesome notification !",
-			rounded: false,
-			variant: "oreo",
-		},
-	},
-];
-
+const Nutella = [];
+let Update = () => [console.log("Nutella is not ready yet")];
 function NutellaSection(props) {
 	const items = Nutella.filter(item => item.position === props.position);
 	return (
 		<div className={props.position}>
-			{items.map((item, key) => {
-				console.log(item);
-				return <AndroidNotification {...item.data} key={key} />;
-			})}
+			{items.map((item, key) => (
+				<AndroidNotification {...item.data} key={key}>
+					{item.actions.map((action, key) => (
+						<React.Fragment key={key}>{action}</React.Fragment>
+					))}
+				</AndroidNotification>
+			))}
 		</div>
 	);
 }
 
-function View() {
-	return (
-		<React.Fragment>
-			<NutellaSection position="top" />
-			<NutellaSection position="bottom" />
-			<NutellaSection position="left" />
-			<NutellaSection position="right" />
-		</React.Fragment>
-	);
+class View extends React.Component {
+	componentDidMount() {
+		Update = () => {
+			this.forceUpdate();
+		};
+	}
+	render() {
+		return (
+			<React.Fragment>
+				<NutellaSection position="top" />
+				<NutellaSection position="bottom" />
+				<NutellaSection position="left" />
+				<NutellaSection position="right" />
+			</React.Fragment>
+		);
+	}
 }
 
 const Interface = {
-	push: (position, data) => {
+	push: (position, data, actions) => {
 		Nutella.push({
 			position,
 			data,
+			actions,
 		});
+		Update();
 	},
 };
 
