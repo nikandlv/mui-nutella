1fb9d2d3bedde8b37683fa2c6af1c23377d4bf1d
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-15 13:27:11 +0430

    Removed the redundent component

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,6 +2,6 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 import NotificationBuilder from "../lib/NotificationBuilder.es";
 
-storiesOf("Demos", module)
-	.add("Notification Builder", () => <NotificationBuilder title="Hello World!" />)
-	.add("aNotification Builder", () => <div>test</div>);
+storiesOf("Demos", module).add("Notification Builder", () => (
+	<NotificationBuilder title="Hello World!" />
+));
